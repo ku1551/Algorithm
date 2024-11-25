8f459a3f0da3829c74ea9bc11c421a5420b21fe8
jin-kyu-kim <jinkyu.kim@vtw.co.kr>
2024-11-26 00:53:25 +0900

    Algorithem

---
+++ src/main/java/com/example/leetCode/Executor.java
@@ -0,0 +1,13 @@
+package com.example.leetCode;
+
+public class Executor {
+    public static void main(String[] args) {
+
+        Arrays arr = new Arrays();
+
+        String adena = "adena software";
+
+        int result = arr.hammingWeight(11);
+
+    }
+}
---
+++ src/main/java/com/example/realTest/Kakao.java
@@ -0,0 +1,7 @@
+package com.example.realTest;
+
+public class Kakao {
+    public void getRange(){
+         
+    }
+}
