5f67fcc3fb0753361e28965fcdcc38d221e67d68
ku1551@hanmail.net <Tlqkf12!!@>
2024-11-30 12:58:59 +0900

    leetcode 322. Coin Change

--- src/main/java/com/example/leetCode/Dp.java
+++ src/main/java/com/example/leetCode/Dp.java
@@ -20,4 +20,20 @@ public class Dp {
 
         return dp[num] = getNum(dp, num-1) + getNum(dp, num-2);
     }
+
+    public int coinChange(int[] coins, int amount) {// 322. Coin Change
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,-1);
+        dp[0] = 0;
+
+        for(int i = 1; i <= amount; i++){
+            for(int j = 0; j < coins.length; j++){
+                if(i - coins[j] >= 0){
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] != amount + 1 ? dp[amount] : -1;
+    }
 }
