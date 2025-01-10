c8318909b281a06e6241e5e90e1d0e83b49725fb
jin-kyu-kim <ku1551@hanmail.net>
2025-01-10 11:22:38 +0900

    Algorithem

--- src/main/java/com/example/leetCode/Arrays.java
+++ src/main/java/com/example/leetCode/Arrays.java
@@ -522,6 +522,57 @@ public class Arrays {
         return true;
     }
 
+    public ListNode reverseList(ListNode head) {
+        // return reverse(head, null);
+        ListNode prev = null, node = head;
+
+        while(node != null){
+            ListNode next = node.next;
+
+            node.next = prev;
+
+            prev = node;
+
+            node = next;
+        }
+        return prev;
+    }
+
+    private ListNode reverse(ListNode node, ListNode prev){
+        if(node == null) return prev;
+
+        ListNode next = node.next;
+
+        node.next = prev;
+
+        return reverse(next, node);
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry > 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+
+        return result.next;
+    }
 
 
 
