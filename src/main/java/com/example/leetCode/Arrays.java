b7e28af3727064e39521ad7b16ef6de84173f7e9
jin-kyu-kim <ku1551@hanmail.net>
2025-01-16 01:26:49 +0900

    Algorithem

--- src/main/java/com/example/leetCode/Arrays.java
+++ src/main/java/com/example/leetCode/Arrays.java
@@ -621,6 +621,38 @@ public class Arrays {
         return stack.isEmpty();
     }
 
+    public String removeDuplicateLetters(String s) {
+        for(char c : toSortedSet(s)){
+            String suffix = s.substring(s.indexOf(c));
+
+            if(toSortedSet(s).equals(toSortedSet(suffix))){
+                return c + removeDuplicateLetters(suffix.replace(String.valueOf(c), ""));
+            }
+        }
+        return "";
+    }
+
+    private Set<Character> toSortedSet(String s){
+        Set<Character> set = new TreeSet<>(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                if(o1 == o2){
+                    return 0;
+                } else if (o1 > o2) {
+                    return 1;
+                }else{
+                    return -1;
+                }
+            }
+        });
+
+        for(int i=0; i<s.length(); i++){
+            set.add(s.charAt(i));
+        }
+
+        return set;
+    }
+
 
 
 
