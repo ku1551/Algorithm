d92b43a36726a6df740d02fe2519cc36fee93f82
jin-kyu-kim <ku1551@hanmail.net>
2025-01-17 01:08:46 +0900

    Algorithem

--- src/main/java/com/example/leetCode/Arrays.java
+++ src/main/java/com/example/leetCode/Arrays.java
@@ -653,6 +653,16 @@ public class Arrays {
         return set;
     }
 
+    public boolean isAnagram(String s, String t) {
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+
+        java.util.Arrays.sort(arr1);
+        java.util.Arrays.sort(arr2);
+
+        return java.util.Arrays.equals(arr1, arr2);
+    }
+
 
 
 
