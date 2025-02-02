304743835ab46c84e5e6dcc853e1933080ee73fe
jin-kyu-kim <ku1551@hanmail.net>
2025-02-03 02:29:32 +0900

    Algorithem

--- src/main/java/com/example/leetCode/Arrays.java
+++ src/main/java/com/example/leetCode/Arrays.java
@@ -806,7 +806,39 @@ public class Arrays {
         }
     }
 
+    public boolean exist(char[][] board, String word) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        boolean result = false;
 
+        for(int i=0; i< board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j] == word.charAt(0)) {
+                    result = findWord(board, visited, word, i, j, 0);
+                    if(result) return true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean findWord(char[][] board, boolean[][] visited, String word, int i, int j, int idx){
+        if(idx == word.length()) return true;
+
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(idx)) return false;
+
+        visited[i][j] = true;
+        idx++;
+        if(findWord(board, visited, word, i+1, j, idx) ||
+           findWord(board, visited, word, i-1, j, idx) ||
+           findWord(board, visited, word, i, j+1, idx) ||
+           findWord(board, visited, word, i, j-1, idx)
+        ){
+            return true;
+        }
+
+        visited[i][j] = false;
+        return false;
+    }
 
 
 }
