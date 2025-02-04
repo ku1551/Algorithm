9c3c480e98872923d4b6ebc21145f5bf30064dad
jin-kyu-kim <ku1551@hanmail.net>
2025-02-05 03:16:59 +0900

    Algorithem

--- src/main/java/com/example/leetCode/Arrays.java
+++ src/main/java/com/example/leetCode/Arrays.java
@@ -857,6 +857,23 @@ public class Arrays {
         return maxLength;
     }
 
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26];
+        int left = 0, maxLength = 0, maxFreq = 0;
+
+        for(int right=0; right<s.length(); right++){
+            count[s.charAt(right) - 'A']++;
+            maxFreq = Math.max(count[s.charAt(right)-'A'], maxFreq);
+
+            while((right-left+1) -maxFreq>k){
+                count[s.charAt(left)-'A']--;
+                left++;
+            }
+            maxLength = Math.max(maxLength, right-left+1);
+        }
+        return maxLength;
+    }
+
 
 }
 
