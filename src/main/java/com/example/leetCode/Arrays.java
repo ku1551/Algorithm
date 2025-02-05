be5c74885f2f876a8cf831c651f417d2cf5ca288
jin-kyu-kim <ku1551@hanmail.net>
2025-02-06 00:41:50 +0900

    Algorithem

--- src/main/java/com/example/leetCode/Arrays.java
+++ src/main/java/com/example/leetCode/Arrays.java
@@ -874,6 +874,26 @@ public class Arrays {
         return maxLength;
     }
 
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if(p == null && q == null) return true;
+//
+//        boolean left = false, right = false;
+//
+//        if (p != null && q != null && p.val == q.val) {
+//            left = isSameTree(p.left, q.left);
+//            right = isSameTree(p.right, q.right);
+//        }
+//
+//        return left && right;
+//    }
+// 최적화
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if(p == null && q == null) return true;
+//        if(p == null || q == null) return false;
+//
+//        return q.val == p.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//    }
+
 
 }
 
